0b5c8a174bb8dae264cb9558157a09a5388d8561
Emmanuel Azunna <emmanuelazunna3@gmail.com>
2022-09-08 20:45:37 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,21 @@
-#include <studio.h>
+#include<studio.h> 
 
-/**
- * main - Entry point 
- *
- * Return: Always 0 (Sucess)
- */
- int main(void)
- {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
- }
+/**                                                                                                                                     
+
+ * main - Entry point                                                                                                                   
+
+ *                                                                                                                                      
+
+ * Return: Always 0 (Sucess)                                                                                                            
+
+ */                                                                                                                                     
+
+ int main(void)                                                                                                                         
+
+ {                                                                                                                                      
+
+        puts("\"Programming is like building a multilingual puzzle");                                                                   
+
+        return (0);                                                                                                                     
+
+ }              
