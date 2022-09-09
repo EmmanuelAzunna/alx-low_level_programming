10f3693980e0fbd1acd71cf74ba2d2ce0cfe8854
Emmanuel Azunna <emmanuelazunna3@gmail.com>
2022-09-08 19:40:14 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,27 +1,11 @@
-#include <studio.h>                                                                                                                     
-
-                                                                                                                                        
-
-/**                                                                                                                                     
-
-* main - print the string in the put function                                                                                           
-
-*                                                                                                                                       
-
-* Description: using the main function                                                                                                  
-
-* this program prints "Programming is like building a multilingual puzzle                                                               
-
-* Return: 0                                                                                                                             
-
-*/                                                                                                                                      
-
-int main(void)                                                                                                                          
-
-{                                                                                                                                       
-
-        puts("\"Programming is like building a multilingual puzzle");                                                                   
-
-        return (0);                                                                                                                     
-
-}                          
+#include <studio.h>
+/**
+* main - Entry point 
+*
+* Return: always 0
+*/
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
