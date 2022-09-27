7277c1ba90825846d2547d164e211f0b91aa2bc0
Emmanuel Azunna <emmanuelazunna3@gmail.com>
2022-09-27 07:46:41 -0700

    0-memset.c

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,12 +1,3 @@
-#ifndef MAIN_H
-#define MAIN_H
-
-/**
-
-* void prototypes()
-* int prototypes()
-*/
-
 int _putchhar(char);
 char *_memset(char *s, char b, unsigned int n);
 char *_mempcy(char *dest, char *src, unsigned int n);
@@ -16,6 +7,4 @@ char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
-void set_string(char **s, char *to);
-
-#endif /*MAIN_H*/
+void set_string(char **s, char *to)
